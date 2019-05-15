dddb20edb7325d8a1e1493fd347769a1c75073d4
Alim Abay <abaev.alim@yandex.ru>
2019-05-15 19:03:52 +0300

    added more tests

--- PomidoroTimerAutotests/tst_test.cpp
+++ PomidoroTimerAutotests/tst_test.cpp
@@ -14,6 +14,9 @@ public:
 private slots:
 	void test_count_possible_repetitions0();
 	void test_count_possible_repetitions1();
+	void test_count_possible_repetitions2();
+	void test_count_possible_repetitions3();
+	void test_count_possible_repetitions4();
 
 };
 
@@ -38,6 +41,27 @@ void Test::test_count_possible_repetitions1()
 	int res = Utility::count_possible_repetitions(20, 3, 15, QTime::fromString("22:16", "hh:mm"));
 
 	QCOMPARE(res, 1);
+}
+
+void Test::test_count_possible_repetitions2()
+{
+	int res = Utility::count_possible_repetitions(20, 3, 15, QTime::fromString("22:15", "hh:mm"));
+
+	QCOMPARE(res, 1);
+}
+
+void Test::test_count_possible_repetitions3()
+{
+	int res = Utility::count_possible_repetitions(25, 5, 30, QTime::fromString("21:35", "hh:mm"));
+
+	QCOMPARE(res, 1);
+}
+
+void Test::test_count_possible_repetitions4()
+{
+	int res = Utility::count_possible_repetitions(25, 5, 30, QTime::fromString("21:36", "hh:mm"));
+
+	QCOMPARE(res, 0);
 }
 
 QTEST_APPLESS_MAIN(Test)
