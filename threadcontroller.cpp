d3c5186a938e051bec1a82256a4b11c315da8b30
Alim Abay <abaev.alim@yandex.ru>
2019-07-19 21:04:36 +0300

    add pomidoro class to threadcontroller

--- threadcontroller.cpp
+++ threadcontroller.cpp
@@ -1,6 +1,17 @@
 #include "threadcontroller.h"
 
-ThreadController::ThreadController(QObject *parent) : QObject(parent)
-{
+#include <QThread>
 
+#include "pomidoro.h"
+
+ThreadController::ThreadController(QObject* parent) : QObject(parent),
+	thread_(new QThread)
+{
+}
+
+void ThreadController::initPomidoro()
+{
+	pomidoro_ = new Pomidoro;
+
+	pomidoro_->moveToThread(thread_);
 }
--- threadcontroller.h
+++ threadcontroller.h
@@ -3,15 +3,25 @@
 
 #include <QObject>
 
+class Pomidoro;
+
 class ThreadController : public QObject
 {
- Q_OBJECT
+	Q_OBJECT
+
 public:
- explicit ThreadController(QObject *parent = nullptr);
+	explicit ThreadController(QObject* parent = nullptr);
 
 signals:
 
 public slots:
+
+	void initPomidoro();
+
+private:
+	QThread* thread_;
+
+	Pomidoro* pomidoro_;
 };
 
 #endif // THREADCONTROLLER_H
